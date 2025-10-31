3da7dc07f1096d543b3d5c47fe25c21c1d7a888a
sabiliwafa29 <rosyadsiwa04@gmail.com>
2025-10-31 23:12:34 +0700

    add vercel.json

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -114,7 +114,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="text-center">
             <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Kenapa Memilih Paradise Of Indonesia?</h2>
-            <p class="mt-4 max-w-2xl mx-auto text-xl text-gray-500">Kami menyediakan layanan terbaik untuk perjalanan Anda</p>
+            <p class="mt-4 max-w-2xl mx-auto text-xl text-gray-500">One click, Heaven in your hands</p>
         </div>
 
         <div class="mt-12 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -34,7 +34,7 @@
                 <h3 class="text-xl font-semibold mb-4">Contact Us</h3>
                 <ul class="space-y-2">
                     <li class="text-gray-400"><i class="fas fa-phone mr-2"></i>WhatsApp+62 8158 5333 325</li>
-                    <li class="text-gray-400"><i class="fas fa-envelope mr-2"></i>info@paradiseofindonesia.com</li>
+                    <li class="text-gray-400"><i class="fas fa-envelope mr-2"></i>cs@paradiseofindonesia.com</li>
                     <li class="text-gray-400"><i class="fas fa-map-marker-alt mr-2"></i>Alamat : Jalan Gading Karya 2 No 1 Kel. Gading Kecamatan Tambaksari kota Surabaya  Jawa Timur</li>
                 </ul>
                 <div class="mt-4 flex space-x-4">
