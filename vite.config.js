fc8f040848f165669995fd9ce60079e12dbea714
sabiliwafa29 <rosyadsiwa04@gmail.com>
2025-11-03 00:14:22 +0700

    vite update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: process.env.VITE_ASSET_URL ? process.env.VITE_ASSET_URL + '/' : '/',
     plugins: [
         laravel({
             input: [
