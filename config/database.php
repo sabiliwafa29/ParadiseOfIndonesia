e293da47189c320309f30d567719127b66f803a2
sabiliwafa29 <rosyadsiwa04@gmail.com>
2025-11-02 22:45:22 +0700

    .

--- config/database.php
+++ config/database.php
@@ -76,7 +76,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => prefer,
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
