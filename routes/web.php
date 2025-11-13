a266f6b76214abb6855d731e6ee5d9dadb54859f
sabiliwafa29 <rosyadsiwa04@gmail.com>
2025-11-14 06:27:28 +0700

    up webroutes

--- routes/web.php
+++ routes/web.php
@@ -100,11 +100,14 @@ Route::middleware('auth')->group(function () {
         
         // Admin Dashboard
         Route::get('dashboard', function () {
-            return redirect()->route('adnin.dashboard')
+            return redirect()->route('admin.dashboard')
                 ->with('login_success', true)
                 ->with('user_name', auth()->user()->name);
         },
         [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
+
+        Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])
+        ->name('dashboard');
         
         // Tour Management
         Route::resource('tours', App\Http\Controllers\Admin\TourController::class);
