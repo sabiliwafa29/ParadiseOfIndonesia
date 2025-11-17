eb86826349c9a182e1c22c49e07f522db826c0c9
sabiliwafa29 <rosyadsiwa04@gmail.com>
2025-11-18 01:23:46 +0700

    Add routes for rescheduling bookings

--- routes/web.php
+++ routes/web.php
@@ -96,6 +96,8 @@ Route::middleware('auth')->group(function () {
         Route::post('/bookings/{tour}', [BookingController::class, 'store'])->name('bookings.store');
         Route::get('/bookings/{booking}', [BookingController::class, 'show'])->name('bookings.show');
         Route::post('/bookings/{booking}/cancel', [BookingController::class, 'cancel'])->name('bookings.cancel');
+            Route::get('/bookings/{booking}/reschedule', [BookingController::class, 'reschedule'])->name('bookings.reschedule');
+    Route::post('/bookings/{booking}/reschedule', [BookingController::class, 'updateReschedule'])->name('bookings.update-reschedule');
     });
     
     /*
