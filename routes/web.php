cbed2b9beb8f1d250009e4d18f0b1e48252c7ab2
sabiliwafa29 <rosyadsiwa04@gmail.com>
2025-11-18 01:21:20 +0700

    Add route for cancelling bookings

--- routes/web.php
+++ routes/web.php
@@ -95,6 +95,7 @@ Route::middleware('auth')->group(function () {
         Route::get('/my-bookings', [BookingController::class, 'index'])->name('my-bookings');
         Route::post('/bookings/{tour}', [BookingController::class, 'store'])->name('bookings.store');
         Route::get('/bookings/{booking}', [BookingController::class, 'show'])->name('bookings.show');
+        Route::post('/bookings/{booking}/cancel', [BookingController::class, 'cancel'])->name('bookings.cancel');
     });
     
     /*
