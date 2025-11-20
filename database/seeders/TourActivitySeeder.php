de21d4f62711d678d6fc743f7f535b6ff1241ca5
sabiliwafa29 <rosyadsiwa04@gmail.com>
2025-11-20 08:18:15 +0700

    Refactor code structure for improved readability and maintainability

--- database/seeders/TourActivitySeeder.php
+++ database/seeders/TourActivitySeeder.php
@@ -44,7 +44,7 @@ class TourActivitySeeder extends Seeder
             'name' => 'Dolphin Dance Lovina Beach',
             'slug' => 'dolphin-dance-lovina-beach',
             'location' => 'Lovina Beach, Bali',
-            'photo' => 'images/activities/lovina_dolphins.jpg',
+            'photo' => 'images/lovinaBeach.jpg',
             'description' => 'Pantai Lovina adalah destinasi wisata terkenal di pesisir utara Bali yang menawarkan pengalaman melihat lumba-lumba liar di habitat aslinya saat matahari terbit. Waktu terbaik adalah antara pukul 05.30 hingga 07.00 pagi.'
         ]);
 
@@ -73,7 +73,7 @@ class TourActivitySeeder extends Seeder
             'name' => 'Blue Fire Ijen Crater',
             'slug' => 'blue-fire-ijen-crater',
             'location' => 'Kawah Ijen, Banyuwangi',
-            'photo' => 'images/activities/ijen_bluefire.jpg',
+            'photo' => 'images/blufireIjenCarter.webp',
             'description' => 'Fenomena Api Biru Kawah Ijen adalah daya tarik alam langka yang hanya dapat ditemukan di dua lokasi di dunia. Api biru terjadi ketika gas belerang bertekanan tinggi terbakar pada suhu ekstrem mencapai 600°C. Waktu terbaik untuk menyaksikannya adalah antara pukul 02.00 hingga 05.00 dini hari.'
         ]);
 
@@ -82,7 +82,7 @@ class TourActivitySeeder extends Seeder
             'name' => 'Mount Bromo Sunrise',
             'slug' => 'mount-bromo-sunrise',
             'location' => 'Gunung Bromo, Probolinggo',
-            'photo' => 'images/activities/bromo_sunrise.jpg',
+            'photo' => 'images/activities/bromo-sunrise.jpg',
             'description' => 'Gunung Bromo menawarkan pengalaman wisata alam yang memukau dengan sunrise ikonik dari Penanjakan, lautan pasir yang luas, Kawah Bromo yang berasap, dan Padang Savana Teletubbies. Spot paling populer untuk menyaksikan matahari terbit spektakuler dengan pemandangan Gunung Bromo, Semeru, dan Batok.'
         ]);
 
@@ -93,6 +93,22 @@ class TourActivitySeeder extends Seeder
             'location' => 'Lumajang, Jawa Timur',
             'photo' => 'images/activities/tumpaksewu.jpg',
             'description' => 'Air Terjun Tumpak Sewu adalah destinasi wisata alam spektakuler yang dijuluki "Niagara Falls-nya Indonesia" dengan formasi air terjun bertingkat setinggi 120 meter. Daya tarik utamanya adalah "Tirai Seribu Air Terjun" dengan ratusan aliran air yang jatuh bersamaan membentuk panorama yang luar biasa indah.'
+        ]);
+        TourActivity::create([
+            'tour_id' => $baliTour->id,
+            'name' => 'Tegalalang, the natural beauty of Ubud',
+            'slug' => 'tegalalang-the-natural-beauty-of-ubud',
+            'location' => 'Ubud, Bali',
+            'photo' => 'images/activities/tegallalangBali.jpg',
+            'description' => 'Tegalalang di Ubud terkenal karena terasering sawahnya yang indah dan menakjubkan, yang diakui sebagai Situs Warisan Dunia UNESCO. Area ini menawarkan kombinasi unik antara keindahan alam, budaya agrikultur tradisional Bali, dan berbagai aktivitas wisata.'
+        ]);
+        TourActivity::create([
+            'tour_id' => $baliTour->id,
+            'name' => 'Kelingking Beach, the iconic cliff of Nusa Penida',
+            'slug' => 'kelingking-beach-the-iconic-cliff-of-nusa-penida',
+            'location' => 'Nusa Penida, Bali',
+            'photo' => 'images/activities/kelingkingBeach.jpg',
+            'description' => 'Kelingking Beach di Nusa Penida terkenal dengan tebing ikoniknya yang menyerupai bentuk T-Rex. Pantai ini menawarkan pemandangan spektakuler, pasir putih, dan air laut yang jernih, menjadi destinasi favorit bagi para wisatawan dan fotografer.'
         ]);
 
         $this->command->info('Tour Activities seeded successfully!');
