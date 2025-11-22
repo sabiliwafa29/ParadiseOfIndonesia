d45b85bb5925ba4bde887fc8d98494dd7b285f54
16Aisyah <aisyahrhmn1103@gmail.com>
2025-11-23 04:16:18 +0700

    Enhance parseDayData to support multiple activity detection with numeric or text time patterns

--- app/Helpers/ItineraryHelper.php
+++ app/Helpers/ItineraryHelper.php
@@ -270,8 +270,8 @@ class ItineraryHelper
                     ?? '';
                 
                 if (!empty($desc)) {
-                    // Check if description contains multiple activities with time patterns
-                    if (preg_match_all('/\[(\d{1,2}[:.]\d{2})/u', $desc) > 1) {
+                    // Check if description contains multiple activities with time patterns (numeric or text)
+                    if (preg_match_all('/\[[^\]]+\]/u', $desc) > 1) {
                         $activities = self::splitMultiActivityString($desc);
                     } else {
                         $activities = [$desc];
@@ -280,9 +280,9 @@ class ItineraryHelper
             }
             // Case 3: Array with 'description' key
             elseif (is_array($dayData) && isset($dayData['description'])) {
-                // Check if description contains multiple activities
+                // Check if description contains multiple activities (numeric or text time)
                 $desc = strval($dayData['description']);
-                if (preg_match_all('/\[(\d{1,2}[:.]\d{2})/u', $desc) > 1) {
+                if (preg_match_all('/\[[^\]]+\]/u', $desc) > 1) {
                     $activities = self::splitMultiActivityString($desc);
                 } else {
                     $activities = [$desc];
@@ -295,8 +295,8 @@ class ItineraryHelper
             // Case 5: Direct string
             else {
                 $stringData = strval($dayData);
-                // Check if string contains multiple activities
-                if (preg_match_all('/\[(\d{1,2}[:.]\d{2})/u', $stringData) > 1) {
+                // Check if string contains multiple activities (numeric or text time)
+                if (preg_match_all('/\[[^\]]+\]/u', $stringData) > 1) {
                     $activities = self::splitMultiActivityString($stringData);
                 } else {
                     $activities = [$stringData];
