97e0cfa32138763ee2af8cbd54c38e1800f70d95
16Aisyah <aisyahrhmn1103@gmail.com>
2025-11-23 07:53:35 +0700

    Refactor sidebar toggle button to use template for SVG icons, improving rendering efficiency

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -47,12 +47,16 @@
                     <button @click="sidebarOpen = !sidebarOpen" 
                             class="text-white bg-emerald-600 hover:bg-emerald-700 transition-all flex-shrink-0 p-2 rounded-lg shadow-md hover:shadow-lg"
                             title="Toggle Sidebar">
-                        <svg x-show="sidebarOpen" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" style="display: none;" x-transition>
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7"/>
-                        </svg>
-                        <svg x-show="!sidebarOpen" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" style="display: none;" x-transition>
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7"/>
-                        </svg>
+                        <template x-if="sidebarOpen">
+                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7"/>
+                            </svg>
+                        </template>
+                        <template x-if="!sidebarOpen">
+                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7"/>
+                            </svg>
+                        </template>
                     </button>
                 </div>
             </div>
